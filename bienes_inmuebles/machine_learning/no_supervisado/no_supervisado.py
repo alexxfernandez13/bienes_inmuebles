20ec45fe71febbdac59dbff5de81bc2d675a0ff4
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2020-12-21 19:43:57 +0100

    adding clustering function

--- bienes_inmuebles/machine_learning/no_supervisado/no_supervisado.py
+++ bienes_inmuebles/machine_learning/no_supervisado/no_supervisado.py
@@ -1,5 +1,6 @@
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
 
 from bienes_inmuebles.dataset.csv_plot import CSVPlot
 
@@ -18,6 +19,14 @@ class NO_Supervisado(CSVPlot):
 
     def unam(self):
         pass
+    """Clustering"""
+    def kmeans_clustering(self,n_cluster=2, random_state=0):
+        kmeans = KMeans(n_clusters=n_cluster, random_state=random_state).fit(self.df)
+        labels = kmeans.labels_
+        #kmeans.predict([[0, 0], [12, 3]])
+        centroide = kmeans.cluster_centers_
+        return kmeans, labels, centroide
+
 
 if __name__ == "__main__":
     no_supervisado = NO_Supervisado("../../data/csv_barcelona.csv")
--- bienes_inmuebles/main.py
+++ bienes_inmuebles/main.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from bienes_inmuebles.dataset.csv_preprocesamiento import CSV, PATH4  # Importa clase csv y variable (CONSTANTE) PATH4
 from bienes_inmuebles.machine_learning.no_supervisado.no_supervisado import NO_Supervisado
@@ -38,19 +39,20 @@ def main():
     print(binar1.df[0:5, 0:5])
     no_supervisado = NO_Supervisado(binar1.df)
     print(no_supervisado.df)
-    breakpoint()
-    pca = no_supervisado.tsne()
-    print(pca)
-    no_supervisado.df = pd.DataFrame(pca)
-    print("a")
-    no_supervisado.plot()
-
-
+    pca = no_supervisado.pca()
+    # print(pca)
+    #no_supervisado.df = pd.DataFrame(pca)
+    # print("a")
+    kmeans, labels, centroide = no_supervisado.kmeans_clustering(n_cluster=3)
+    # print(kmeans)
+    #no_supervisado.df = pd.DataFrame(kmeans)
+    # no_supervisado.plot()
+    plt.scatter(pca[:, 0],pca[:, 1],c=labels)
+    plt.show()
 
 """ COMMAND LINE / EJECUTAS LA FILE DIRECTO"""
 if __name__ == "__main__":
     main()
-
 
 """
 csv = CSV("datos.csv")
