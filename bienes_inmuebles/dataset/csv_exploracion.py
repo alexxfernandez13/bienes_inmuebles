e69da6aae826b289736f08cf4ad30a41a22f03d6
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2021-01-05 14:44:42 +0100

    cambios

--- bienes_inmuebles/dataset/csv_exploracion.py
+++ bienes_inmuebles/dataset/csv_exploracion.py
@@ -1,4 +1,12 @@
 import pandas as pd
+
+## Para RFE
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+
+## Para importancia de la característica
+from sklearn.ensemble import ExtraTreesClassifier
+
 from bienes_inmuebles.dataset.csv_plot import CSVPlot
 
 
@@ -6,18 +14,18 @@ class CSVExploracion(CSVPlot):
 
     def __init__(self, df):
         self.df = df
-    """Muestra informacion general del dataset con show_info=True o especifica con show_info=False"""
-    def vistazo(self):  # ¿Pasar lineas predeterminadas?
+    """Muestra informacion general del dataset"""
+    def vistazo(self):
         info = self.df.info()
         cabecera = self.df.head()
         final = self.df.tail()
         columnas = self.df.columns.values
         faltantes = self.df.isnull().sum()
         forma = self.df.shape
-        print(f'\nCABECERA \n{cabecera} \n FINAL \n {final} \n COLUMNAS \n{columnas} \nFALTANTES \n{faltantes} \nFORMA \n{forma}')
+        print(f'\nCABECERA:\n{cabecera}\n\nFINAL:\n{final}\n\nCOLUMNAS:\n{columnas}\n\nMVs:\n{faltantes}\n\nFORMA:\n{forma}')
         return info, cabecera, final, columnas, faltantes, forma
 
-    """Muestra informacion estadistica del dataset. Se le puede indicar columnas especificas pasandolas por una lista"""
+    """Muestra informacion estadistica del dataset. Indicando columnas por una lista realiza ademas la agrupacion"""
     def estadistica(self, columnas=[], agrupar=None, method="pearson"):
         if columnas:
             df = self.df[columnas]
@@ -30,11 +38,30 @@ class CSVExploracion(CSVPlot):
         describir = df.describe()
         correlaciones = df.corr(method=method)
         sesgo = df.skew()
-        print(agrupar, describir, correlaciones, sesgo)
+        print(f'\nAGRUPAR:\n{agrupar}\n\nDESCRIBIR:\n{describir}\n\nCORRELACIONES:\n{correlaciones}\n\nSESGO:\n{sesgo}')
         return agrupar, describir, correlaciones, sesgo
 
+    """Indica columnas importantes del Dataset para ML"""
+    def caracteristicas(self, n_caracteristicas=3):
+        print(self.df.columns.values)
+        X = self.df[:, 0:8]
+        Y = self.df[:, 8]
+
+        # 2.Extracción de características con RFE
+        # modelo = LogisticRegression(solver='lbfgs')
+        modelo = SGDClassifier()
+        rfe = RFE(modelo, n_caracteristicas)
+        fit = rfe.fit(X, Y)
+        print(f"Numero de Caracteristicas Seleccionadas por RFE: {fit.n_features_}\n")
+        print(f"Columnas del Dataset: {self.df.columns.values}")
+        print(f"Caracteristicas Seleccionadas:\n{fit.support_}\nRanking de Caracteristicas:\n{fit.ranking_}")
+
+        # 4.Extracción de importancia de las características
+        modelo = ExtraTreesClassifier()
+        fit = modelo.fit(X, Y)
+        print(f"Importancia de caracteristicas: {fit.feature_importances_}")
 
 if __name__ == "__main__":
-    df = pd.read_csv("../../data/datos_fotocasa.txt", sep=",")
+    df = pd.read_csv("../../data/csv_barcelona.csv")
     exploracion = CSVExploracion(df)
-    exploracion.vistazo()
+    exploracion.caracteristicas()
