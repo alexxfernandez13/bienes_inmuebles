ae4b6df79b4adc29076b5c31b811879470363175
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2021-01-05 12:44:53 +0100

    cambios exploracion

--- bienes_inmuebles/dataset/csv_exploracion.py
+++ bienes_inmuebles/dataset/csv_exploracion.py
@@ -7,18 +7,15 @@ class CSVExploracion(CSVPlot):
     def __init__(self, df):
         self.df = df
     """Muestra informacion general del dataset con show_info=True o especifica con show_info=False"""
-    def vistazo(self, show_info=False):  # ¿Pasar lineas predeterminadas?
-        if show_info:
-            self.df.info()
-        else:
-            pass
+    def vistazo(self):  # ¿Pasar lineas predeterminadas?
+        info = self.df.info()
         cabecera = self.df.head()
         final = self.df.tail()
         columnas = self.df.columns.values
         faltantes = self.df.isnull().sum()
         forma = self.df.shape
-        print(cabecera, final, columnas, faltantes, forma)
-        return cabecera, final, columnas, faltantes, forma
+        print(f'\nCABECERA \n{cabecera} \n FINAL \n {final} \n COLUMNAS \n{columnas} \nFALTANTES \n{faltantes} \nFORMA \n{forma}')
+        return info, cabecera, final, columnas, faltantes, forma
 
     """Muestra informacion estadistica del dataset. Se le puede indicar columnas especificas pasandolas por una lista"""
     def estadistica(self, columnas=[], agrupar=None, method="pearson"):
@@ -38,6 +35,6 @@ class CSVExploracion(CSVPlot):
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("../../data/csv_barcelona.csv")
+    df = pd.read_csv("../../data/datos_fotocasa.txt", sep=",")
     exploracion = CSVExploracion(df)
-    exploracion.vistazo(show=True)
+    exploracion.vistazo()
