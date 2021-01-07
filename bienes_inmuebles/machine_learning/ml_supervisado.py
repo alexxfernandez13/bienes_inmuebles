349c316cd4924764ee4c7db4026c8c4121c318b3
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2021-01-07 11:09:18 +0100

    cambios ml

--- bienes_inmuebles/machine_learning/ml_supervisado.py
+++ bienes_inmuebles/machine_learning/ml_supervisado.py
@@ -48,9 +48,10 @@ from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKF
 
 import warnings
 
+
 class Supervisado(CSVPlot):
 
-    def __init__(self, csv, size=0.33,seed=42,num_folds=10, scoring = 'accuracy'):
+    def __init__(self, csv, size=0.33, seed=42, num_folds=10, scoring='accuracy'):
         self.csv = csv
         self.df = pd.read_csv(self.csv)
         self.array = self.df.values
@@ -66,7 +67,7 @@ class Supervisado(CSVPlot):
                                                                         random_state=self.seed)
         return X_train, X_validation, Y_train, Y_validation
 
-    def pipeline_clasificacion_normales(self):
+    def normales(self):
         # Usaremos 6 tipos:
         models = []
         models.append(('LR', LogisticRegression(solver='lbfgs', random_state=self.seed)))
@@ -77,19 +78,7 @@ class Supervisado(CSVPlot):
         models.append(('SVM', SVC(gamma='scale', random_state=self.seed)))
         return models
 
-    def pipeline_clasificacion_estandarizados(self):
-        pipelines = []
-        pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()), (
-        'LR', LogisticRegression(solver='lbfgs', max_iter=500, random_state=self.seed))])))
-        pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()), ('LDA', LinearDiscriminantAnalysis())])))
-        pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsClassifier())])))
-        pipelines.append(('ScaledCART', Pipeline(
-            [('Scaler', StandardScaler()), ('CART', DecisionTreeClassifier(random_state=self.seed))])))
-        pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])))
-        pipelines.append(
-            ('ScaledSVC', Pipeline([('Scaler', StandardScaler()), ('SVC', SVC(gamma='scale', random_state=self.seed))])))
-        return pipelines
-    def pipeline_emsembles(self):
+    def ensembles(self):
         # Usaremos 6 tipos:
         models = []
         models.append(('AB', AdaBoostClassifier(random_state=self.seed)))
@@ -99,20 +88,69 @@ class Supervisado(CSVPlot):
         models.append(('ET', ExtraTreesClassifier(random_state=self.seed)))
         return models
 
-    def evaluacion(self,models, X_train, Y_train):
+    def estandarizados(self):
+        pipelines = []
+        pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()), (
+            'LR', LogisticRegression(solver='lbfgs', max_iter=500, random_state=self.seed))])))
+        pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()), ('LDA', LinearDiscriminantAnalysis())])))
+        pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsClassifier())])))
+        pipelines.append(('ScaledCART', Pipeline(
+            [('Scaler', StandardScaler()), ('CART', DecisionTreeClassifier(random_state=self.seed))])))
+        pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])))
+        pipelines.append(
+            (
+                'ScaledSVC',
+                Pipeline([('Scaler', StandardScaler()), ('SVC', SVC(gamma='scale', random_state=self.seed))])))
+        return pipelines
+
+    def ensembles_estandarizados(self):
+        ensembles = []
+        ensembles.append(
+            ('ScaledAB', Pipeline([('Scaler', StandardScaler()), ('AB', AdaBoostClassifier(random_state=self.seed))])))
+        ensembles.append(
+            ('ScaledGBM',
+             Pipeline([('Scaler', StandardScaler()), ('GBM', GradientBoostingClassifier(random_state=self.seed))])))
+        ensembles.append(
+            ('ScaledBC', Pipeline([('Scaler', StandardScaler()), ('BC', BaggingClassifier(random_state=self.seed))])))
+        ensembles.append(
+            ('ScaledRF',
+             Pipeline([('Scaler', StandardScaler()), ('RF', RandomForestClassifier(random_state=self.seed))])))
+        ensembles.append(
+            (
+                'ScaledET',
+                Pipeline([('Scaler', StandardScaler()), ('ET', ExtraTreesClassifier(random_state=self.seed))])))
+        return ensembles
+
+    def voting(self, X, Y):
+        estimadores = []
+        modelo1 = LogisticRegression(solver='lbfgs', random_state=self.seed)
+        estimadores.append(("LR", modelo1))
+        modelo2 = DecisionTreeClassifier(random_state=self.seed)
+        estimadores.append(('CART', modelo2))
+        modelo3 = BaggingClassifier(random_state=self.seed)
+        estimadores.append(('BC', modelo3))
+
+        # Creamos el modelo ensemble
+        ensemble = VotingClassifier(estimadores)
+        nombre = "Voting Ensemble"
+        resultados = cross_val_score(ensemble, X, Y, cv=self.kfold)
+        print(f"\n{nombre}: {round(resultados.mean(), 2)} ({round(resultados.std(), 2)})")
+        return nombre, resultado
+
+    def evaluacion(self, models, X_train, Y_train):
         # Evaluamos cada modelo por turnos
         results = []
         names = []
         print("\nLos Resultados son:")
         for name, model in models:
-            kfold = KFold(n_splits=self.num_folds, random_state=self.seed)
-            cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=self.scoring)
+            self.kfold = KFold(n_splits=self.num_folds, random_state=self.seed)
+            cv_results = cross_val_score(model, X_train, Y_train, cv=self.kfold, scoring=self.scoring)
             results.append(cv_results)
             names.append(name)
-            print(f"{name}: {round(cv_results.mean(),2)} ({round(cv_results.std(),2)})")
-        return results, names
+            print(f"{name}: {round(cv_results.mean(), 2)} ({round(cv_results.std(), 2)})")
+        return names, results
 
-    def comparar_plot (self, results, names):
+    def comparar_plot(self, names, results):
         # Escogemos el más preciso, que en este caso será SVM
         fig = pyplot.figure()
         fig.suptitle('Comparación de Algoritmos')
@@ -126,6 +164,23 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     dataset = Supervisado('../../data/18. visitasUsuarios.csv')
     X_train, X_validation, Y_train, Y_validation = dataset.split_out()
-    modelos = dataset.pipeline_clasificacion_normales()
-    resultado, nombre = dataset.evaluacion(modelos, X_train,Y_train)
-    dataset.comparar_plot(resultado, nombre)
+
+    # Normales
+    modelos_normales = dataset.normales()
+    nombre, resultado = dataset.evaluacion(modelos_normales, X_train, Y_train)
+    dataset.comparar_plot(nombre, resultado)
+
+    modelos_ensembles = dataset.ensembles()
+    nombre, resultado = dataset.evaluacion(modelos_ensembles, X_train, Y_train)
+    dataset.comparar_plot(nombre, resultado)
+
+    dataset.voting(dataset.X, dataset.Y)
+
+    # Estandarizados
+    estandar = dataset.estandarizados()
+    nombre, resultado = dataset.evaluacion(estandar, X_train, Y_train)
+    dataset.comparar_plot(nombre, resultado)
+
+    ensembles_estandar = dataset.ensembles_estandarizados()
+    nombre, resultado = dataset.evaluacion(ensembles_estandar, X_train, Y_train)
+    dataset.comparar_plot(nombre, resultado)
---
+++ bienes_inmuebles/machine_learning/modelos.py
@@ -0,0 +1,3 @@
+class models():
+    def __init__(self):
+        pass
