6652a51cdd973637c1f8d76b4327a2662eebf5bb
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2021-01-19 16:05:32 +0100

    modelos module changed

---
+++ bienes_inmuebles/machine_learning/eval.py
@@ -0,0 +1,14 @@
+
+
+class Evaluation():
+
+    def __init__(self, pred, y_true):
+        self.pred = pred
+        self.y_true = y_true
+
+    def confusion_matrix(self):
+
+
+    def eval(self, scoring):
+
+
--- bienes_inmuebles/machine_learning/modelos.py
+++ bienes_inmuebles/machine_learning/modelos.py
@@ -3,38 +3,39 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
-class Model():
 
-    def __init__(self, clf):
+class Model(Evaluation):
+
+    def __init__(self, clf, X, Y):
         self.clf = clf
-        self.X = None
-        self.Y = None
-        self.clf_optimizado = None
-
-    ###DED DONDE VIENEN LOS DATOS?? CSV --> PREPROCES --> TrainTestSplitter
-    def inicializar(self,X, Y):
         self.X = X
         self.Y = Y
+        self.clf_optimizado = None
+        self.best_score= None
 
-    def optimizacion(self , parametros):
-        if self.X and self.Y:
-            parametros = parametros
-            #grid = GridSearch()
-            #grid.fit(self.clf, self.X, self.Y)
-            #self.clf_optimizado = grid._best_parameters
-            return self.clf.optimizado
+    ###DED DONDE VIENEN LOS DATOS?? CSV --> PREPROCES --> TrainTestSplitter
+
+    #argumentos posicionales y argunmentos no posicionales
+    def optimizacion(self, parametros, cv=10, scoring='f1_weighted'):
+
+        if self.X.tolist() and self.Y.tolist() and parametros:
+
+            grid = GridSearchCV(self.clf, parametros, scoring=scoring, cv=cv)
+            grid.fit(self.X, self.Y)
+            self.clf_optimizado = grid.best_estimator_
+            self.best_score = grid.best_score_
+            return self.clf_optimizado, self.best_score
         else:
-            print("Run method fit first")
-    def evaluar(self):
-        pass
+            print("No ha corrido la optimizacion")
+            return self.clf, ""
 
-    def voting(self):
-        pass
-
-    def predict(self, X):
+    def predict(self, X_test):
         if self.clf_optimizado:
-            pred = self.clf_optimizado.predict(X)
+            pred = self.clf_optimizado.predict(X_test)
             return pred
         else:
             print("Run optimizacion method first")
@@ -42,15 +43,23 @@ class Model():
 
 if __name__ == "__main__":
     df = pd.read_csv('./18. visitasUsuarios.csv')
+    X_columns = df[['PeriodA', 'PeriodB']].values
+    Y_columns = df["Equipo"].values
+    X_train, X_test, y_train, y_test = train_test_split(X_columns, Y_columns, test_size=0.3, random_state=42)
+    modelos_parametros = {KNeighborsClassifier(): {'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]}}
+    for classificador, parametros in modelos_parametros.items():
+        modelo = Model(classificador, X_train, y_train)
+        model, score =modelo.optimizacion(parametros) #train -> train/validation -> score
+        y_pred = modelo.predict(X_test) #test -> pred(test) == y_test???
+        print("Validation score", score) # scoring con los datos entrenados
+        print("Test score", accuracy_score(y_test, y_pred))
+        print("Test score", confusion_matrix(y_test, y_pred)) # scoring en situacion real
 
+    #escoger 3 mejores modelos
 
-    X_train, y_train, X_test, y_test = train_test_split.split_out(df, siz=0.3, score="accuracy")
-"""
-    for modelo in []:
-        modelo = Model(KNeighborsClassifier())
-        modelo.fit(X_train, y_train)
-        modelo.optimizacion(parametro_kneighbours)
-        modelo.predict(X_test)"""
+    #Voting 3 mejores
+    voting = Voting(clf[0], clf[1], clf[2])
+    Modelo(voting)
 
 """
 1) Split datos
