bb38e9ee0ffe3b29911b14330d4c289e5461787a
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2021-01-24 12:29:09 +0100

    Update eval.py

--- bienes_inmuebles/machine_learning/eval.py
+++ bienes_inmuebles/machine_learning/eval.py
@@ -1,5 +1,4 @@
 
-
 class Evaluation():
 
     def __init__(self, pred, y_true):
@@ -7,8 +6,7 @@ class Evaluation():
         self.y_true = y_true
 
     def confusion_matrix(self):
-
+        pass
 
     def eval(self, scoring):
-
-
+        pass
