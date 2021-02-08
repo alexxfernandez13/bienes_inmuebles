b8cd99fcf600660b4cce900f3bd247c967b3518b
alexxfernandez13 <alexxfernandez13@users.noreply.github.com>
2021-02-08 17:43:28 +0100

    nuevo main

--- bienes_inmuebles/machine_learning/prediccion.py
+++ bienes_inmuebles/machine_learning/prediccion.py
@@ -3,6 +3,7 @@ import os
 from bienes_inmuebles.dataset.csv_preprocesamiento import PATH4
 
 def main():
-    modelo = load('filename.joblib')
+    modelo = load(os.path.join(PATH4, "data/filename.joblib"))
+    print(modelo)
 if __name__ == "__main__":
     main()
