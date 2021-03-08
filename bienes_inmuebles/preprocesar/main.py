33f54de333c2210e4bccc38070ad4873ed1a22ff
alexxfernandez13 <alexxfernandez@hotmail.es>
2021-03-08 15:13:55 +0100

    fix changes

--- bienes_inmuebles/preprocesar/main.py
+++ bienes_inmuebles/preprocesar/main.py
@@ -8,23 +8,17 @@ from bienes_inmuebles.preprocesar.csv_abrir import CSV
 from bienes_inmuebles.preprocesar.csv_preprocesamiento import PATH4
 from bienes_inmuebles.machine_learning.supervisado import prepare_dataset, regresion
 from sklearn.ensemble import GradientBoostingRegressor
-from bienes_inmuebles.utilidades.urlPath import UrlPath
 
 
 def main():
+    """**************************************** FASE DE PREPROCESAMIENTO ****************************************"""
     """Carga de CSV y configura la informacion por pantalla de Pandas mostrando TODOS los atributos"""
     csv = CSV(os.path.join(PATH4, "data/datos_fotocasa_final.csv"))
     pd.set_option('display.max_columns', None)
 
     """Casteo Atributos en Enteros"""
-    casteo_variables = {'precio': np.float64,
-                        'tamano': np.float64,
-                        'trastero': np.int64,
-                        'balcon': np.int64,
-                        'aireAcondicinado': np.int64,
-                        'piscina': np.int64,
-                        'ascensor': np.int64,
-                        'terraza': np.int64,
+    casteo_variables = {'precio': np.float64, 'tamano': np.float64, 'trastero': np.int64, 'balcon': np.int64,
+                        'aireAcondicinado': np.int64, 'piscina': np.int64, 'ascensor': np.int64, 'terraza': np.int64,
                         'planta': np.int64}
     csv_casteados = csv.casteo_columnas(casteo_variables)
 
@@ -39,144 +33,71 @@ def main():
     csv_na = csv_dup.dropna(number=10, axis=0)
     csv_int = csv_na.ints()
     csv_mvs = csv_int.mvs()
-    # csv_outlier = csv_int.outliers()
 
     """Casteo Atributos en Enteros"""
-    casteo_variables = {'tipoInmueble': np.int64,
-                        'tipoOperacion': np.int64,
-                        'habitaciones': np.int64,
-                        'tamano': np.int64,
-                        'planta': np.int64,
-                        'ascensor': np.int64,
-                        'terraza': np.int64,
-                        'trastero': np.int64,
-                        'balcon': np.int64,
-                        'aireAcondicinado': np.int64,
-                        'piscina': np.int64,
-                        'banos': np.int64,
-                        'garaje_Comunitario': np.int64,
-                        'garaje_No-detallado': np.int64,
-                        'garaje_Privado': np.int64,
-                        'distrito_arganzuela': np.int64,
-                        'distrito_barajas': np.int64,
-                        'distrito_carabanchel': np.int64,
-                        'distrito_centro': np.int64,
-                        'distrito_chamartin': np.int64,
-                        'distrito_chamberi': np.int64,
-                        'distrito_ciudad-lineal': np.int64,
-                        'distrito_fuencarral': np.int64,
-                        'distrito_hortaleza': np.int64,
-                        'distrito_latina': np.int64,
-                        'distrito_moncloa': np.int64,
-                        'distrito_moratalaz': np.int64,
-                        'distrito_puente-de-vallecas': np.int64,
-                        'distrito_retiro': np.int64,
-                        'distrito_salamanca': np.int64,
-                        'distrito_san-blas': np.int64,
-                        'distrito_tetuan': np.int64,
-                        'distrito_usera': np.int64,
-                        'distrito_vicalvaro': np.int64,
-                        'distrito_villa-de-vallecas': np.int64,
-                        'distrito_villaverde': np.int64,
-                        'ciudad_madrid-capital': np.int64,
-                        'eficienciaEnergetica_A': np.int64,
-                        'eficienciaEnergetica_B': np.int64,
-                        'eficienciaEnergetica_C': np.int64,
-                        'precio': np.int64}
+    casteo_variables = {'tipoInmueble': np.int64, 'tipoOperacion': np.int64, 'habitaciones': np.int64,
+                        'tamano': np.int64, 'planta': np.int64, 'ascensor': np.int64, 'terraza': np.int64,
+                        'trastero': np.int64, 'balcon': np.int64, 'aireAcondicinado': np.int64, 'piscina': np.int64,
+                        'banos': np.int64, 'garaje_Comunitario': np.int64, 'garaje_No-detallado': np.int64,
+                        'garaje_Privado': np.int64, 'distrito_arganzuela': np.int64, 'distrito_barajas': np.int64,
+                        'distrito_carabanchel': np.int64, 'distrito_centro': np.int64, 'distrito_chamartin': np.int64,
+                        'distrito_chamberi': np.int64, 'distrito_ciudad-lineal': np.int64,
+                        'distrito_fuencarral': np.int64, 'distrito_hortaleza': np.int64, 'distrito_latina': np.int64,
+                        'distrito_moncloa': np.int64, 'distrito_moratalaz': np.int64,
+                        'distrito_puente-de-vallecas': np.int64, 'distrito_retiro': np.int64,
+                        'distrito_salamanca': np.int64, 'distrito_san-blas': np.int64, 'distrito_tetuan': np.int64,
+                        'distrito_usera': np.int64, 'distrito_vicalvaro': np.int64,
+                        'distrito_villa-de-vallecas': np.int64, 'distrito_villaverde': np.int64,
+                        'ciudad_madrid-capital': np.int64, 'eficienciaEnergetica_A': np.int64,
+                        'eficienciaEnergetica_B': np.int64, 'eficienciaEnergetica_C': np.int64, 'precio': np.int64}
 
+    csv_procesado = csv_mvs.casteo_columnas(casteo_variables)
 
-    csv_casteados_2_vez = csv_mvs.casteo_columnas(casteo_variables)
-
-    csv_casteados_2_vez.vistazo()
-    """Analitica Descriptiva"""
-    print("\n------------------------ Analisis Dataset ------------------------")
-    csv_mvs.vistazo()
-    print("\n------------------------ Estadistica Dataset ------------------------")
-    csv_mvs.estadistica()
-
-    """Plots"""
-    #csv_mvs.borrar_output()
-    #csv_mvs.plot_histograma()
-    #csv_mvs.plot_densidad()
-
-    #csv_mvs.plot_bigotes(plot_columnas=["precio",'distrito_centro'])
-
-    #csv_mvs.plot_correlacion()
-    #csv_mvs.plot_dispersion()
-
-    """Separar datos en 2 Dataframe, uno para compra y otro para alquiler"""
-    csv_compra = copy(csv_mvs)
-    csv_alquiler = copy(csv_mvs)
-
-    csv_compra.df = csv_mvs.df.loc[
-        (csv_mvs.df['tipoOperacion'] == 1) & (csv_mvs.df['precio'] >= 50000) & (csv_mvs.df["tamano"] >= 10)]
-    csv_alquiler.df = csv_mvs.df.loc[
-        (csv_mvs.df["tipoOperacion"] == 2) & (csv_mvs.df["precio"] <= 9000) & (csv_mvs.df["precio"] >= 100) & (
-                csv_mvs.df["tamano"] >= 10)]
-
-    csv = csv.casteo_columnas(casteo_variables)
-    csv_dup = csv.duplicados()
-    csv_na = csv_dup.dropna(number=10, axis=0)
-    csv_mvs = csv_na.mvs()
-
-# --------------------- COMPRA ---------------------
-    csv_compra = copy(csv)
-    csv_compra.df = csv.df.loc[(csv.df['tipoOperacion'] == 1)]
-
-    print(csv_compra.df.shape)
-
-    """csv_compra.plot_bigotes(plot_columnas=['banos'])
-    csv_compra.plot_bigotes(plot_columnas=['habitaciones'])
-    csv_compra.plot_bigotes(plot_columnas=['planta'])
-    csv_compra.plot_bigotes(plot_columnas=['tamano'])
-    csv_compra.plot_bigotes(plot_columnas=['precio'])"""
-
-    csv_compra__out = copy(csv_compra)
-    csv_compra__out.outliers_nuevo(q1=0.25, q2=0.75)
-
-    print(csv_compra__out.df.shape)
-
-    """csv_compra__out.plot_bigotes(plot_columnas=['banos'])
-    csv_compra__out.plot_bigotes(plot_columnas=['habitaciones'])
-    csv_compra__out.plot_bigotes(plot_columnas=['planta'])
-    csv_compra__out.plot_bigotes(plot_columnas=['tamano'])
-    csv_compra__out.plot_bigotes(plot_columnas=['precio'])"""
-
-    #csv_compra__out.scatter_2variables(var1='tamano', var2='precio')
-
-
-
-    # creacion de csv a partir de dataframe de compra
-    if os.path.exists(str(PATH4) + "\data\csv_compra.csv"):
-        os.remove(str(PATH4) + "\data\csv_compra.csv")
-    csv_compra.df.to_csv(str(PATH4) + "\data\csv_compra.csv", index=False)
-
-    # creacion de csv a partir de dataframe de alquiler
-    if os.path.exists(str(PATH4) + "\data\csv_alquiler.csv"):
-        os.remove(str(PATH4) + "\data\csv_alquiler.csv")
-    csv_alquiler.df.to_csv(str(PATH4) + "\data\csv_alquiler.csv", index=False)
-
-    # creacion de csv a partir de dataframe total (comprar y alquilar
+    """Output CSV procesado"""
     if os.path.exists(str(PATH4) + "\data\csv_preprocesado.csv"):
         os.remove(str(PATH4) + "\data\csv_preprocesado.csv")
     csv_mvs.df.to_csv(str(PATH4) + "\data\csv_preprocesado.csv", index=False)
 
+    """Analitica Descriptiva"""
+    print("\n------------------------ Analisis Dataset ------------------------")
+    csv_procesado.vistazo()
+    print("\n------------------------ Estadistica Dataset ------------------------")
+    csv_procesado.estadistica()
+
+    """Plots"""
+    csv_procesado.borrar_output()
+    csv_procesado.plot_histograma()
+    csv_procesado.plot_densidad()
+    csv_procesado.plot_bigotes(plot_columnas=["precio", 'distrito_centro'])
+    csv_procesado.plot_correlacion()
+    csv_procesado.plot_dispersion()
+
+    """Separar datos en 2 Dataframe, uno para compra y otro para alquiler"""
+    csv_compra = copy(csv_procesado)
+    csv_alquiler = copy(csv_procesado)
+
+    csv_compra.df = csv_procesado.df.loc[(csv_procesado.df['tipoOperacion'] == 1) &
+                                         (csv_procesado.df['precio'] >= 50000) & (csv_procesado.df["tamano"] >= 10)]
+
+    csv_alquiler.df = csv_procesado.df.loc[(csv_procesado.df["tipoOperacion"] == 2) &
+                                           (csv_procesado.df["precio"] <= 9000) & (
+                                                   csv_procesado.df["precio"] >= 100) & (
+                                                   csv_procesado.df["tamano"] >= 10)]
+
+    """Outliers"""
+
+    """**************************************** FASE DE MODELADO ****************************************"""
     """COMPRA - Separar X e Y"""
     # Estandarizar Columna X --> Optimiza el Modelado
-    X_columns_df_compra = csv_compra.df[['tipoInmueble', 'tipoOperacion', 'habitaciones',
-                                         'tamano', 'planta', 'ascensor', 'terraza', 'trastero', 'balcon',
-                                         'aireAcondicinado', 'piscina', 'banos', 'garaje_Comunitario',
-                                         'garaje_No-detallado', 'garaje_Privado', 'distrito_arganzuela',
-                                         'distrito_barajas', 'distrito_carabanchel', 'distrito_centro',
-                                         'distrito_chamartin', 'distrito_chamberi',
-                                         'distrito_ciudad-lineal', 'distrito_fuencarral',
-                                         'distrito_hortaleza', 'distrito_latina', 'distrito_moncloa',
-                                         'distrito_moratalaz', 'distrito_puente-de-vallecas',
-                                         'distrito_retiro', 'distrito_salamanca', 'distrito_san-blas',
-                                         'distrito_tetuan', 'distrito_usera', 'distrito_vicalvaro',
-                                         'distrito_villa-de-vallecas', 'distrito_villaverde',
-                                         'ciudad_madrid-capital', 'eficienciaEnergetica_A',
-                                         'eficienciaEnergetica_B', 'eficienciaEnergetica_C']]
+    X_columns_df_compra = csv_compra.df[
+        ['tipoInmueble', 'tipoOperacion', 'habitaciones', 'tamano', 'planta', 'ascensor', 'terraza', 'trastero',
+         'balcon', 'aireAcondicinado', 'piscina', 'banos', 'garaje_Comunitario', 'garaje_No-detallado',
+         'garaje_Privado', 'distrito_arganzuela', 'distrito_barajas', 'distrito_carabanchel', 'distrito_centro',
+         'distrito_chamartin', 'distrito_chamberi', 'distrito_ciudad-lineal', 'distrito_fuencarral',
+         'distrito_hortaleza', 'distrito_latina', 'distrito_moncloa', 'distrito_moratalaz',
+         'distrito_puente-de-vallecas', 'distrito_retiro', 'distrito_salamanca', 'distrito_san-blas', 'distrito_tetuan',
+         'distrito_usera', 'distrito_vicalvaro', 'distrito_villa-de-vallecas', 'distrito_villaverde',
+         'ciudad_madrid-capital', 'eficienciaEnergetica_A', 'eficienciaEnergetica_B', 'eficienciaEnergetica_C']]
     csv_Compra_X = CSV(df=X_columns_df_compra)
     csv_Compra_X_estandarizada = csv_Compra_X.estandarizar(os.path.join(PATH4, "data/scaler_compra.pkl"))
     X_columns_Compra = csv_Compra_X_estandarizada.df.values
@@ -187,7 +108,7 @@ def main():
     """COMPRA - Evaluacion Modelos"""
     X_train, X_test, y_train, y_test = prepare_dataset(X_columns_Compra, Y_columns_Compra)
     print("\n------------------------ Scoring Modelos - Compra ------------------------")
-    # regresion(X_train, X_test, y_train, y_test)
+    regresion(X_train, X_test, y_train, y_test)
     print("------------------------ Scoring Modelos - Compra ------------------------")
 
     """COMPRA - Guardar Modelo Seleccionado"""
@@ -197,20 +118,15 @@ def main():
 
     """ALQUILER - Separar X e Y"""
     # Estandarizar Columna X --> Optimiza el Modelado
-    X_columns_df_alquier = csv_alquiler.df[['tipoInmueble', 'tipoOperacion', 'habitaciones',
-                                            'tamano', 'planta', 'ascensor', 'terraza', 'trastero', 'balcon',
-                                            'aireAcondicinado', 'piscina', 'banos', 'garaje_Comunitario',
-                                            'garaje_No-detallado', 'garaje_Privado', 'distrito_arganzuela',
-                                            'distrito_barajas', 'distrito_carabanchel', 'distrito_centro',
-                                            'distrito_chamartin', 'distrito_chamberi',
-                                            'distrito_ciudad-lineal', 'distrito_fuencarral',
-                                            'distrito_hortaleza', 'distrito_latina', 'distrito_moncloa',
-                                            'distrito_moratalaz', 'distrito_puente-de-vallecas',
-                                            'distrito_retiro', 'distrito_salamanca', 'distrito_san-blas',
-                                            'distrito_tetuan', 'distrito_usera', 'distrito_vicalvaro',
-                                            'distrito_villa-de-vallecas', 'distrito_villaverde',
-                                            'ciudad_madrid-capital', 'eficienciaEnergetica_A',
-                                            'eficienciaEnergetica_B', 'eficienciaEnergetica_C']]
+    X_columns_df_alquier = csv_alquiler.df[
+        ['tipoInmueble', 'tipoOperacion', 'habitaciones', 'tamano', 'planta', 'ascensor', 'terraza', 'trastero',
+         'balcon', 'aireAcondicinado', 'piscina', 'banos', 'garaje_Comunitario', 'garaje_No-detallado',
+         'garaje_Privado', 'distrito_arganzuela', 'distrito_barajas', 'distrito_carabanchel', 'distrito_centro',
+         'distrito_chamartin', 'distrito_chamberi', 'distrito_ciudad-lineal', 'distrito_fuencarral',
+         'distrito_hortaleza', 'distrito_latina', 'distrito_moncloa', 'distrito_moratalaz',
+         'distrito_puente-de-vallecas', 'distrito_retiro', 'distrito_salamanca', 'distrito_san-blas', 'distrito_tetuan',
+         'distrito_usera', 'distrito_vicalvaro', 'distrito_villa-de-vallecas', 'distrito_villaverde',
+         'ciudad_madrid-capital', 'eficienciaEnergetica_A', 'eficienciaEnergetica_B', 'eficienciaEnergetica_C']]
     csv_Alquiler_X = CSV(df=X_columns_df_alquier)
     csv_Alquiler_X_estandarizada = csv_Alquiler_X.estandarizar(os.path.join(PATH4, "data/scaler_alquiler.pkl"))
     X_columns_Alquiler = csv_Alquiler_X_estandarizada.df.values
@@ -221,7 +137,7 @@ def main():
     """ALQUILER - Evaluacion Modelos"""
     X_train, X_test, y_train, y_test = prepare_dataset(X_columns_Alquiler, Y_columns_Alquiler)
     print("\n------------------------ Scoring Modelos - Alquiler ------------------------")
-    # regresion(X_train, X_test, y_train, y_test)
+    regresion(X_train, X_test, y_train, y_test)
     print("------------------------ Scoring Modelos - Alquiler ------------------------")
 
     """Alquiler - Guardar Modelo Seleccionado"""
